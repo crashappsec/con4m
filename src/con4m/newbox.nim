241192cebc464d349e0c03046df0382a5fe4200c
John Viega <john@zork.org>
2022-12-22 14:01:22 -0500

    Make dollar work for lists and dicts.  Add toJson function for boxes.

--- src/con4m/newbox.nim
+++ src/con4m/newbox.nim
@@ -3,8 +3,10 @@
 # due to him.
 
 import std/typetraits
+import std/hashes
 import strutils
 import tables
+import json
 
 # This is the clever trick to get the type of the array/seq item...
 # call lastType to decompose.
@@ -14,8 +16,9 @@ proc lastType[T](a: openarray[T]): auto =
   else:
     default(T)
 
+
 type
-  MixedKind = enum
+  MixedKind* = enum
     MkInt, MkStr, MkObj, MkFloat, MkSeq, MkBool
   BoxAtom = string or int64 or float64 or RootRef or Box
   BoxHoldsAtoms = concept x
@@ -28,14 +31,17 @@ type
     of MkBool: b: bool
     of MkStr: s: string
     of MkSeq: c: ListCrate
-    of MkObj:
+    of MkObj: # Only used for tables.
       o: RootRef
-      typename: string
-  SomeTableRef = TableRef or OrderedTableRef or CountTableRef
+      baseType: string
+      keyType: string
+      valType: string
+  SomeTableRef = TableRef or OrderedTableRef
 
   ListCrate = ref object of RootObj
     s: seq[Box]
-    typename: string
+    typeName: string
+    itemType: string
 
   TableCrate[T: SomeTableRef] = ref object of RootObj
     t: T
@@ -74,16 +80,53 @@ proc unpack*[T](box: Box, result: var T) =
       unpack[typeof(result.lastType)](item, x)
       result.add(x)
   elif typeof(result) is SomeTableRef:
-    let tc: TableCrate[T] = cast[TableCrate[T]](box.o)
-    result = tc.t
+    let tc = cast[TableCrate[OrderedTableRef[Box, Box]]](box.o)
+    var
+      genericParamDummy: genericParams(result.type)
+      dstKey: genericParamDummy[0].type()
+      dstVal: genericParamDummy[1].type()                                        
+      
+    when T is OrderedTableRef:
+      result = newOrderedTable[genericParamDummy[0].type(),
+                               genericParamDummy[1].type()]()
+    else:
+      result = newTable[genericParamDummy[0].type(),
+                        genericParamDummy[1].type()]()
+    for k, v in tc.t:
+      unpack[genericParamDummy[0].type()](k, dstKey)
+      unpack[genericParamDummy[1].type()](v, dstVal)
+      result[dstKey] = dstVal
   else:
     raise newException(ValueError, "Bad type to unpack: " & $(T.type))
 
 proc unpack*(box: BoxAtom): auto =
   result = box
 
+proc hash*(box: Box): Hash =
+  case box.kind
+  of MkInt:
+    var unboxed: int
+    unpack[int](box, unboxed)
+    return hash(unboxed)
+  of MkStr:
+    var unboxed: string
+    unpack[string](box, unboxed)
+    return hash(unboxed)
+  of MkFloat:
+    var unboxed: float
+    unpack[float](box, unboxed)
+    return hash(unboxed)
+  of MkBool:
+    var unboxed: bool
+    unpack[bool](box, unboxed)
+    return hash(unboxed)
+  else:
+    raise newException(ValueError, "Invalid type for hash key")
+    
 proc pack*[T](x: T): Box =
-  when T is SomeInteger:
+  when T is Box:
+    result = x
+  elif T is SomeInteger:
     result = Box(kind: MkInt, i: cast[int64](x))
   elif T is SomeFloat:
     result = Box(kind: MkFloat, f: cast[float64](x))
@@ -98,13 +141,27 @@ proc pack*[T](x: T): Box =
     result = Box(kind: MkSeq, c: c)
     for item in x:
       c.s.add(item)
+  elif T is seq:
+    let c = ListCrate(s: newSeq[Box](), typename: $(x.type))
+    result = Box(kind: MkSeq, c: c)
+    for item in x:
+      c.s.add(pack(item))
   elif T is SomeTableRef:
+    let newdict: OrderedTableRef[Box, Box] = newOrderedTable[Box, Box]()
+    var kt = ""
+    var vt = ""      
+    for k, v in x:
+      if kt == "":
+        kt = $(k.type)
+        vt = $(v.type)
+      newdict[pack(k)] = pack(v)
     result = Box(kind: MkObj,
-                 o: TableCrate[typeof(x)](t: x),
-                 typename: $(x.type))
+                 o: TableCrate[OrderedTableRef[Box, Box]](t: newDict),
+                 baseType: "TableRef",
+                 keyType: kt,
+                 valType: vt)
   else:
     raise newException(ValueError, "Bad type to pack: " & $(T.type))
-    
 
 proc `$`*(x: Box): string =
   case x.kind
@@ -113,7 +170,15 @@ proc `$`*(x: Box): string =
   of MkInt:
     return $(x.i)
   of MkObj:
-    return x.typename
+    let c = cast[TableCrate[OrderedTableRef[Box, Box]]](x.o)
+    var addComma: bool = false
+
+    result = "{"
+    for k, val in c.t:
+      if addComma: result = result & ", " else: addComma = true
+      result = result & $(k) & " : " & $(val)
+    
+    result = result & "}"
   of MkBool:
     return $(x.b)
   of MkStr:
@@ -123,6 +188,28 @@ proc `$`*(x: Box): string =
     for item in x.c.s:
       s.add($(item))
     return "mix[" & s.join(", ") & "]"
+
+proc boxToJson*(b: Box): string =
+  var addComma: bool = false
+  
+  case b.kind
+  of MkInt, MkFloat, MkBool:
+    return $(b)
+  of MkStr:
+    return escapeJson($(b))
+  of MkSeq:
+    result = "["
+    for item in b.c.s:
+      if addComma: result = result & ", ": else: addComma = true
+      result = result & item.boxToJSon()
+    result = result & "]"
+  of MkObj:
+    let
+      c = cast[TableCrate[OrderedTableRef[Box, Box]]](b.o)
+    for k, val in c.t:
+      if addComma: result = result & ", " else: addComma = true
+      result = result & boxToJson(k) & " : " & boxToJson(val)
+    result = result & "}"
 
 when isMainModule:
   var
@@ -153,18 +240,41 @@ when isMainModule:
 
   echo outseq
 
-  var myDict = newTable[string, int]()
+  var myDict = newTable[string, seq[string]]()
 
-  myDict["foo"] = 12
+  
+  myDict["foo"] = @["a", "b"]
+  myDict["bar"] = @["b"]
+  myDict["boz"] = @["c"]
+  myDict["you"] = @["d"]
+
+  import streams
+  
+  let
+    f = newFileStream("box.nim", fmRead)
+    contents = f.readAll()[0 .. 20]
+
+  myDict["file"] = @[contents]
 
   let dictBox = pack(myDict)
 
-  var newDict: TableRef[string, int]
+  let listbox = pack(a)
 
-  echo dictBox
+  var outList = unpack(listbox)
+
+  echo "Here's the listbox: ", listbox
+  echo "Here it is unpacked: ", outlist
+
+  var newDict: TableRef[string, seq[string]]
+
   unpack(dictBox, newDict)
 
-  echo newDict
+  echo "Here's the dictbox: ", dictBox
+
+  echo "Here it is unpacked: ", newDict
+
+  echo "Here it is, boxed, as Json: ", boxToJson(dictBox)
+  
 
   # import sugar
   # This shoul fail
