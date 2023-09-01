fbbefd154f06b4596abf6426afbc82701ede75f2
John Viega <john@zork.org>
2023-08-31 22:14:44 -0400

    ...

--- files/bin/buildlibs.sh
+++ files/bin/buildlibs.sh
@@ -8,7 +8,6 @@ BASEDIR=${1}/files
 cd ${BASEDIR}
 
 echo "basedir = ${BASEDIR}"
-ls deps/lib/
 
 if [[ ${ARCH} = "x86_64" ]] ; then
     NIMARCH=amd64
@@ -24,7 +23,7 @@ else
     OS=linux
 fi
 
-DEPS_DIR=${BASEDIR}/deps
+DEPS_DIR=${BASEDIR}/files/deps
 DEP_LIB=${DEPS_DIR}/lib/${OS}-${NIMARCH}
 DEP_SRC=${DEPS_DIR}/src
 DEP_USR=${DEPS_DIR}/usr
