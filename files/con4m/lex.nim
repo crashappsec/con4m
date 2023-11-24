5c055f23ec2d19f40b3dc52daaf9275f6d8df597
John Viega <viega@Johns-MacBook-Pro.local>
2023-11-23 21:59:20 -0500

    tmp

--- files/con4m/lex.nim
+++ files/con4m/lex.nim
@@ -3,12 +3,12 @@
 ## :Author: John Viega (john@crashoverride.com)
 ## :Copyright: 2022
 
-import unicode, nimutils, types, strcursor, errmsg
+import unicode, nimutils, types, errmsg, strcursor, style
 
 template lexFatal(msg: string, t: Con4mToken) =
   var st = when not defined(release): getStackTrace() else: ""
   fatal(msg, t, st, instantiationInfo())
-
+    
 proc uEsc(s: seq[Rune], numchars: int, t: Con4mToken): Rune =
   var
     c = 0
@@ -516,7 +516,7 @@ proc lex*(s: StringCursor, filename: string = ""): (bool, seq[Con4mToken]) =
         tok(TtCharLit)
     of Rune('"'):
       var tristring = false
-
+      
       if s.peek() == Rune('"'):
         s.advance()
         if s.peek() == Rune('"'):
@@ -524,7 +524,7 @@ proc lex*(s: StringCursor, filename: string = ""): (bool, seq[Con4mToken]) =
           tristring = true
         else:
           tok(TtStringLit, 1)
-          continue # Back to the top
+          continue
       while true:
         let r = s.read()
         case r
@@ -541,7 +541,6 @@ proc lex*(s: StringCursor, filename: string = ""): (bool, seq[Con4mToken]) =
             continue
           s.advance()
           tok(TtStringLit, 3)
-          tristring = false
           break
         of Rune('\x00'):
           tok(ErrorStringLit)
@@ -560,62 +559,161 @@ proc lex*(s: StringCursor, filename: string = ""): (bool, seq[Con4mToken]) =
       tok(TtEOF)
       return (true, processStrings(toks))
     else:
-      var r: Rune
+      var 
+        r: Rune
+        litmod = false
+
       if uint(c) < 128:
-        r = Rune(c)
+        r = c
       else:
         s.setPosition(s.getPosition() - 1)
         r = s.read()
-      if not r.isIdStart() and r != Rune('$'):
+      if not r.isIdStart() and r != Rune('$') and r != Rune('?'):
         tok(ErrorTok)
         return (false, toks)
       while true:
         r = s.peek()
-        if r.isIdContinue() or r == Rune('$'):
+        case r
+        of Rune('$'):
           discard s.read()
-        else:
+        of Rune('?'):
+          discard s.read()
           break
-
-      case $(s.slice(startPos, s.getPosition()))
-      of "var":            tok(TtVar)
-      of "True", "true":   tok(TtTrue)
-      of "False", "false": tok(TtFalse)
-      of "is":             tok(TtCmp)
-      of "and":            tok(TtAnd)
-      of "or":             tok(TtOr)
-      of "not":            tok(TtNot)
-      of "if":             tok(TtIf)
-      of "elif":           tok(TtElIf)
-      of "else":           tok(TtElse)
-      of "for":            tok(TtFor)
-      of "from":           tok(TtFrom)
-      of "to":             tok(TtTo)
-      of "break":          tok(TtBreak)
-      of "continue":       tok(TtContinue)
-      of "return":         tok(TtReturn)
-      of "enum":           tok(TtEnum)
-      of "func":           tok(TtFunc)
-      of "export":         tok(TtExportVar)
-      # of "bool":           tok(TtBool)
-      # of "int":            tok(TtInt)
-      # of "char":           tok(TtChar)
-      # of "float":          tok(TtFloat)
-      # of "string":         tok(TtString)
-      # of "void":           tok(TtVoid)
-      # of "list":           tok(TtList)
-      # of "dict":           tok(TtDict)
-      # of "tuple":          tok(TtTuple)
-      # of "typespec":       tok(TtTypeSpec)
-      # of "Duration":       tok(TtDuration)
-      # of "IPAddr":         tok(TtIPAddr)
-      # of "CIDR":           tok(TtCIDR)
-      # of "Size":           tok(TtSize)
-      # of "Date":           tok(TtDate)
-      # of "Time":           tok(TtTime)
-      # of "DateTime":       tok(TtDateTime)
-      else:                tok(TtIdentifier)
+        of Rune('"'), Rune('\''):
+          tok(TtLitMod)
+          litmod = true
+          break
+        of Rune('<'):
+          if s.peek(1) == Rune('<'):
+            tok(TtLitMod)
+            litmod = true
+          break
+        else:
+          if r.isIdContinue():
+            discard s.read()
+          else:
+            break
+          
+      if not litmod:
+        case $(s.slice(startPos, s.getPosition()))
+        of "var":            tok(TtVar)
+        of "True", "true":   tok(TtTrue)
+        of "False", "false": tok(TtFalse)
+        of "is":             tok(TtCmp)
+        of "and":            tok(TtAnd)
+        of "or":             tok(TtOr)
+        of "not":            tok(TtNot)
+        of "if":             tok(TtIf)
+        of "elif":           tok(TtElIf)
+        of "else":           tok(TtElse)
+        of "for":            tok(TtFor)
+        of "from":           tok(TtFrom)
+        of "to":             tok(TtTo)
+        of "break":          tok(TtBreak)
+        of "continue":       tok(TtContinue)
+        of "return":         tok(TtReturn)
+        of "enum":           tok(TtEnum)
+        of "func":           tok(TtFunc)
+        of "export":         tok(TtExportVar)
+        of "object":         tok(TtObject)
+        of "ref":            tok(TtRef)
+        else:                tok(TtIdentifier)
 
   unreachable
 
 proc lex*(s: string, filename: string = ""): (bool, seq[Con4mToken]) =
   return s.newStringCursor().lex(filename)
+
+proc `$`*(tok: Con4mToken): string =
+  case tok.kind
+  of TtStringLit:      result = "\"" & tok.unescaped & "\""
+  of TtCharLit:        result = "'" & $(tok.cursor.slice(tok.startPos, 
+                                                         tok.endPos)) & "'"
+  of TtSof:            result = "~sof~"
+  of TtEof:            result = "~eof~"
+  of ErrorTok:         result = "~err~"
+  of ErrorLongComment: result = "~unterm comment~"
+  of ErrorStringLit:   result = "~unterm string~"
+  of ErrorCharLit:     result = "~bad char lit~"
+  of ErrorOtherLit:    result =  "~unterm other lit~"
+  of TtOtherLit:
+    result = "<<" & $(tok.cursor.slice(tok.startPos, tok.endPos)) & ">>"
+  else:
+    if tok.cursor == nil:
+      return ""
+    echo "Start: ", tok.startPos
+    echo "End: ", tok.endPos
+    echo "cursor: ", tok.cursor
+    echo "Processing token.", tok.startPos, " ", tok.endPos, " ", tok.cursor.runes.len()
+    result = $(tok.cursor.slice(tok.startPos, tok.endPos))
+
+  if tok.litType != "":
+    result = tok.litType & result
+
+proc toRope*(tok: Con4mToken): Rope =
+  result = atom(`$`tok)
+
+  case tok.kind
+  of TtStringLit:
+    result.fgColor(getCurrentCodeStyle().stringLitColor)
+  of TtCharLit:
+    result.fgColor(getCurrentCodeStyle().charLitColor)    
+  of TtIntLit, TtFloatLit:
+    result.fgColor(getCurrentCodeStyle().numericLitColor)
+  of TtTrue, TtFalse:
+    result.fgColor(getCurrentCodeStyle().boolLitColor)
+  of TtOtherLit:
+    result.fgColor(getCurrentCodeStyle().otherLitColor)
+  of TtLitMod:
+    result.fgColor(getCurrentCodeStyle().litModColor)
+  of TtMul, TtDiv, TtMod, TtLte, TtLt, TtGte, TtGt, TtCmp, TtNeq, TtPlus,
+     TtLocalAssign, TtAttrAssign, TtComma, TtPeriod, TtSemi, TtMinus, 
+     TtLockAttr, TtBacktick, TtArrow, TtColon:
+    result.fgColor(getCurrentCodeStyle().operatorColor)
+  of TtLineComment, TtLongComment:
+    result.fgColor(getCurrentCodeStyle().commentColor)
+  of TtLBrace, TtRBrace, TtLBracket, TtRBracket, TtLParen, TtRParen:
+    result.fgColor(getCurrentCodeStyle().otherDelimColor)
+  of TtExportVar, TtNot, TtAnd, TtOr, TtIf, TtElIf, TTElse, TtFor, TtFrom,
+     TtTo, TtBreak, TtContinue, TtReturn, TtEnum, TtFunc, TtVar, TtObject,
+     TtRef:
+    result.fgColor(getCurrentCodeStyle().keywordColor)
+  of TtIdentifier:
+    result.fgColor(getCurrentCodeStyle().identColor)
+  of TtWhiteSpace, TtNewLine, TtSof, TtEof, ErrorTok, ErrorLongComment,
+       ErrorStringLit, ErrorCharLit, ErrorOtherLit:
+    discard
+
+proc toRope*(tokens: seq[Con4mToken]): Rope =
+  var cells: seq[seq[Rope]] = @[@[atom("Number"), atom("Type"), atom("Line"), 
+                                atom("Col"), atom("Value")]]
+
+  for i, item in tokens:
+    var row: seq[Rope] = @[]
+    row.add(atom($(i + 1)))
+    row.add(atom($(item.kind)))
+    row.add(atom($(item.lineNo)))
+    row.add(atom($(item.lineOffset)))
+    row.add(item.toRope())
+    cells.add(row)
+    
+  return quickTable(cells)
+
+when isMainModule:
+  useCrashTheme()
+  import std/marshal
+  let f = "x := x + 2"
+  let (success, tokens) = f.lex()
+
+  if not success:
+    quit()
+
+  print tokens.toRope()
+
+  let marshalled = $$tokens
+
+  print marshalled
+
+  let tokens2 = to[seq[Con4mToken]](marshalled)
+
+  print tokens2.toRope()
